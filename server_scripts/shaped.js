125c6aa5d4445babc27aabb3071d5061fb574be9
Dani <daniverhaar@hotmail.com>
2024-04-23 14:54:56 +0200

    Update shaped.js

--- server_scripts/shaped.js
+++ server_scripts/shaped.js
@@ -1,27 +1,29 @@
-event.shaped("minecraft:red_mushroom_block", [
-    "MMM", 
-    "MMM",
-    "MMM"
-  ], {
-    M: "minecraft:red_mushroom"
-  }
-),
-
-event.shaped("minecraft:brown_mushroom_block", [
-    "MMM", 
-    "MMM",
-    "MMM"   
-  ], {
-    M: "minecraft:brown_mushroom"
-  }
-),
-
-event.shaped("minecraft:mushroom_stem", [
-    "MMM", 
-    "MWM",
-    "MMM"  
-  ], {
-    M: "#farwater:mushroom",
-    W: "minecraft:white_dye"
-  }
-),
+ServerEvents.recipes(event => {
+    event.shaped("minecraft:red_mushroom_block", [
+        "MMM", 
+        "MMM",
+        "MMM"
+      ], {
+        M: "minecraft:red_mushroom"
+      }
+    ),
+    
+    event.shaped("minecraft:brown_mushroom_block", [
+        "MMM", 
+        "MMM",
+        "MMM"   
+      ], {
+        M: "minecraft:brown_mushroom"
+      }
+    ),
+    
+    event.shaped("minecraft:mushroom_stem", [
+        "MMM", 
+        "MWM",
+        "MMM"  
+      ], {
+        M: "#farwater:mushroom",
+        W: "minecraft:white_dye"
+      }
+    ),
+}
