71f2af3afcdc0f960d2481c968325ab1af882123
Spudacious <145444678+Milo-Cat@users.noreply.github.com>
2024-04-26 18:05:53 +0100

    removed burrowing arrow and nuke :'(((

--- server_scripts/removed_items.js
+++ server_scripts/removed_items.js
@@ -1,1 +1,1 @@
-event.remove({ output: 'ars_nouveau:stable_warp_scroll' },{ output: 'ars_nouveau:warp_scroll' })
+event.remove({ output: 'ars_nouveau:stable_warp_scroll' },{ output: 'ars_nouveau:warp_scroll' },{ output: 'alexscaves:burrowing_arrow' },{ output: 'alexscaves:nuclear_bomb' })
