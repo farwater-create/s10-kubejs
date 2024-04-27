eae0aa5d28e72b597169cc584b31f107f5c6835c
Spudacious <145444678+Milo-Cat@users.noreply.github.com>
2024-04-27 20:33:16 +0100

    merged "recipie Removals" and "removed_items"

--- server_scripts/outputRemovals.js
+++ server_scripts/outputRemovals.js
@@ -7,8 +7,15 @@ const recipeRemovals = [
     { output: "cataclysm:ignitium_elytra_chestplate" },
     { output: "cataclysm:ignitium_leggings" },
     { output: "cataclysm:ignitium_boots" },
+    
+    // Ars Nouveau
+     { output: 'ars_nouveau:stable_warp_scroll' },
+    { output: 'ars_nouveau:warp_scroll' },
+    
+    // Alex's caves
+    { output: 'alexscaves:burrowing_arrow' },
+    { output: 'alexscaves:nuclear_bomb'}
 ];
-
 ServerEvents.recipes((e) => {
  	recipeRemovals.forEach((recipe) => {
     	e.remove(recipe);
