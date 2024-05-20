dc5e80e10f0c334a300402621df0a86643f753fc
expdani <daniverhaar@hotmail.com>
2024-05-20 15:21:01 +0200

    runic items

---
+++ startup_scripts/items.js
@@ -0,0 +1,4 @@
+StartupEvents.registry('item', e => {
+  e.create('runic_gold').texture('farwater:item/runic_gold')
+  e.create('incomplete_runic_gold').texture('farwater:item/incomplete_runic_gold').maxStackSize(1)
+})
