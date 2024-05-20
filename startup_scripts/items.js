b09ee141004687ad954eca073855611935d23266
expdani <daniverhaar@hotmail.com>
2024-05-20 17:23:19 +0200

    remove maxstack

--- startup_scripts/items.js
+++ startup_scripts/items.js
@@ -1,4 +1,6 @@
-StartupEvents.registry('item', e => {
-  e.create('runic_gold').texture('farwater:item/runic_gold')
-  e.create('incomplete_runic_gold').texture('farwater:item/incomplete_runic_gold').maxStackSize(1)
-})
+StartupEvents.registry("item", (e) => {
+  e.create("runic_gold").texture("farwater:item/runic_gold");
+  e.create("incomplete_runic_gold").texture(
+    "farwater:item/incomplete_runic_gold"
+  );
+});
