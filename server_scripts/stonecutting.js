cd93c5b8e655b12bfa7f4d7ac098f0d36d1f9eff
Janko <161398674+Janko1902@users.noreply.github.com>
2024-06-01 20:01:19 +0200

    locometal recipes

---
+++ server_scripts/stonecutting.js
@@ -0,0 +1,13 @@
+ServerEvents.recipes(e => {
+    [
+    "railways:slashed_locometal",
+    "railways:riveted_locometal",
+    "railways:locometal_pillar",
+    "railways:locometal_smokebox",
+    "railways:plated_locometal",
+    "railways:flat_slashed_locometal",
+    "railways:flat_riveted_locometal",
+    ].forEach((item) => {
+      e.stonecutting(item, "create:industrial_iron_block");
+    )
+  });
