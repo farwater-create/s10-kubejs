fd9931327226d99989e1c60275cf9ea9c38b0578
Spudacious <145444678+Milo-Cat@users.noreply.github.com>
2024-07-29 23:11:38 +0100

    Systone recipie  (enchanting-apparatus.js)

--- server_scripts/enchanting-apparatus.js
+++ server_scripts/enchanting-apparatus.js
@@ -1,17 +1,16 @@
 ServerEvents.recipes((event) => {
-  //   event.recipes.ars_nouveau.enchanting_apparatus(
-  //     [
-  //       "minecraft:redstone",
-  //       "minecraft:redstone",
-  //       "minecraft:redstone",
-  //       "minecraft:redstone",
-  //       "minecraft:redstone",
-  //       "minecraft:redstone",
-  //       "minecraft:redstone",
-  //       "minecraft:redstone",
-  //     ],
-  //     "minecraft:quartz",
-  //     "create:rose_quartz",
-  //     1000 // may want to alter source cost
-  //   );
+    event.recipes.ars_nouveau.enchanting_apparatus(
+       [
+         "forbidden_arcanus:arcane_crystal_dust_speck",
+         "forbidden_arcanus:arcane_crystal_dust_speck",
+         "forbidden_arcanus:arcane_crystal_dust_speck",
+         "forbidden_arcanus:arcane_crystal_dust_speck",
+         "forbidden_arcanus:arcane_crystal_dust_speck",
+         "forbidden_arcanus:arcane_crystal_dust_speck",
+       ],
+       "create:powdered_obsidian",
+       "ae2:sky_dust",
+       0 // may want to alter source cost
+     );
+
 });
