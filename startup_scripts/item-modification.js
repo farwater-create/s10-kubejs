c90bfcdaafc147922dd66f339f299c3eb9a8bc63
expdani <daniverhaar@hotmail.com>
2024-07-31 09:48:44 +0200

    fix syntax

--- startup_scripts/item-modification.js
+++ startup_scripts/item-modification.js
@@ -13,25 +13,6 @@ ItemEvents.modification((event) => {
     item.burnTime = 3 * 60 * 20;
   });
 
-    let machine = (name) => {
-    let id = name.toLowerCase();
-    event.modify("create:" + id + "_machine", item => {
-    item.maxStackSize = 64
-    item.rarity = 'RARE'
-  })
-  };
-
-  machine("Andesite");
-  machine("Brass");
-  machine("Sealed");
-  machine("Locomotive");
-
-    event.modify('minecraft:honey_block', item => {
-    item.foodProperties = food => {
-        food.hunger(2)
-        food.saturation(3)
-    }
-  
   let manaArmor = [
     ["twilightforest:ironwood_helmet", 10],
     ["twilightforest:ironwood_chestplate", 15],
@@ -43,8 +24,8 @@ ItemEvents.modification((event) => {
     ["twilightforest:steeleaf_leggings", 10],
     ["twilightforest:steeleaf_boots", 5],
 
-    ["twilightforest:phantom_helmet", 40, 2],
-    ["twilightforest:phantom_chestplate", 40, 2],
+    ["twilightforest:phantom_helmet", 60, 2],
+    ["twilightforest:phantom_chestplate", 80, 2],
 
     ["twilightforest:fiery_helmet", 20],
     ["twilightforest:fiery_chestplate", 30, 1],
@@ -72,5 +53,25 @@ ItemEvents.modification((event) => {
         );
       }
     });
+  });
+
+  let machine = (name) => {
+    let id = name.toLowerCase();
+    event.modify("create:" + id + "_machine", (item) => {
+      item.maxStackSize = 64;
+      item.rarity = "RARE";
+    });
+  };
+
+  machine("Andesite");
+  machine("Brass");
+  machine("Sealed");
+  machine("Locomotive");
+
+  event.modify("minecraft:honey_block", (item) => {
+    item.foodProperties = (food) => {
+      food.hunger(2);
+      food.saturation(3);
+    };
   });
 });
