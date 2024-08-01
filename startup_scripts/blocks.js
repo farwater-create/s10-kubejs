654265139e52255289ca69df0db462f220f08f57
Janko <161398674+Janko1902@users.noreply.github.com>
2024-08-01 23:50:38 +0200

    Trophy hitbox

--- startup_scripts/blocks.js
+++ startup_scripts/blocks.js
@@ -3,9 +3,15 @@ StartupEvents.registry("block", (event) => {
     .create("farwater:trophy", "cardinal")
     .displayName("Trophy")
     .material("lantern")
-    .renderType("solid")
+    .renderType("cutout")
+    .soundType("copper")
     .hardness(1.0)
-    .notSolid();
+    .notSolid()
+    .box(5, 0, 5, 11, 3, 11,true)
+    .box(6, 3, 6, 10, 4, 10, true)
+    .box(7, 4, 7, 9, 5, 9, true)
+    .box(6, 5, 6, 10, 6, 10, true)
+    .box(5, 6, 5, 11, 12, 11, true);
 
   let machine = (name, layer) => {
     let id = name.toLowerCase();
