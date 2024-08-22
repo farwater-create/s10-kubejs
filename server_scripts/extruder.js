b6eead3ceedcac7fbc4e045bb56ffe0d691fe30e
Janko <161398674+Janko1902@users.noreply.github.com>
2024-08-22 17:49:47 +0200

    test

--- server_scripts/extruder.js
+++ server_scripts/extruder.js
@@ -18,4 +18,58 @@ ServerEvents.recipes((e) => {
       item: "minecraft:diorite",
     },
   });
+  
+    e.custom({
+    type: "create_mechanical_extruder:extruding",
+    ingredients: [
+      {
+        fluid: "minecraft:water",
+        amount: 1000,
+      },
+      {
+        fluid: "minecraft:lava",
+        amount: 1000,
+      },
+    ],
+    catalyst: {
+      item: "minecraft:tuff",
+    },
+    result: {
+      item: "minecraft:dripstone_block",
+    },
+  });
+
+  e.custom({
+    type: "create_mechanical_extruder:extruding",
+    ingredients: [
+      {
+        fluid: "create:honey",
+        amount: 1000,
+      },
+      {
+        fluid: "minecraft:lava",
+        amount: 1000,
+      },
+    ],
+    result: {
+      item: "create:limestone",
+    },
+  });
+
+  e.custom({
+    type: "create_mechanical_extruder:extruding",
+    ingredients: [
+      {
+        fluid: "create:chocolate",
+        amount: 1000,
+      },
+      {
+        fluid: "minecraft:lava",
+        amount: 1000,
+      },
+    ],
+    result: {
+      item: "create:scoria",
+    },
+  });
 });
