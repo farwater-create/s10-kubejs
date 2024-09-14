c98d08de4324918cbf74cd111234cbd7ab82b396
Janko <161398674+Janko1902@users.noreply.github.com>
2024-09-14 17:11:05 +0200

    add money to loot chests

--- server_scripts/lootjs.js
+++ server_scripts/lootjs.js
@@ -13,4 +13,18 @@ LootJS.modifiers((event) => {
   event
     .addBlockLootModifier(`twilightforest:reappearing_block`)
     .removeLoot(Ingredient.all);
+
+  event
+    .addLootTypeModifier(LootType.CHEST)
+	  .addWeightedLoot(
+	    [0, 5],
+		  [
+	  	Item.of("numismatics:spur").withChance(20),
+	  	Item.of("numismatics:bevel").withChance(10),
+		  Item.of("numismatics:sprocket").withChance(5),
+	  	Item.of("numismatics:cog").withChance(2),
+	  	Item.of("numismatics:crown").withChance(0.5),
+	  	Item.of("numismatics:sun").withChance(0.01),
+	  	]
+  	);
 });
